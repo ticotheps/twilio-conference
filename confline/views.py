08233ed140cd61861da6ce29b1588b73e35a64be
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 05:10:56 -0400

    feat(views): connects the caller to the callee without 'conference'

--- confline/views.py
+++ confline/views.py
@@ -3,22 +3,33 @@ from django.views.decorators.csrf import csrf_exempt
 from decouple import config
 from twilio.twiml.voice_response import Conference, Dial, VoiceResponse, Say
 
-CONF_PIN = config("CONF_PIN")
-MAX_PARTICIPANTS = config("MAX_PARTICIPANTS", default=2, cast=int)
-NUM_DIGITS = config("NUM_DIGITS", default=4, cast=int)
-
 @csrf_exempt
 def my_conference_line(request):
     response = VoiceResponse()
-    dial = Dial()
-    response.say('Thank you for allowing TireTutor to make tire buying easier for you! Please wait while we connect you to your tire dealer now.')
-    dial.conference(
-        'TireTutor Conference Room', 
-        beep=False,
-        wait_url='http://demo.twilio.com/docs/classic.mp3',
-        start_conference_on_enter=True,
-        end_conference_on_exit=True
-    )
-    response.append(dial)
-
+    
+    target_number = config('TARGET_NUMBER')
+    
+    response.say(
+        'Thank you for allowing TireTutor to make tire buying easier ' +
+        'for you! Please wait while we connect you to your tire dealer now!')
+    response.dial(target_number)
+    
     return HttpResponse(str(response))
+
+# @csrf_exempt
+# def my_conference_line(request):
+#     response = VoiceResponse()
+#     dial = Dial()
+#     response.say(
+#         'Thank you for allowing TireTutor to make tire buying easier ' +
+#         'for you! Please wait while we connect you to your tire dealer now.')
+#     dial.conference(
+#         'TireTutor Conference Room', 
+#         beep=False,
+#         wait_url='http://demo.twilio.com/docs/classic.mp3',
+#         start_conference_on_enter=True,
+#         end_conference_on_exit=True
+#     )
+#     response.append(dial)
+
+#     return HttpResponse(str(response))
