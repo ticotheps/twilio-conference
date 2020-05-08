f44aa51c2e603c9304a9d51f5c89c1977d2d1be5
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 01:09:33 -0400

    refactor(settings): replace hard-coded SECRET_KEY with ENV VARS

--- MyConfLine/settings.py
+++ MyConfLine/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '4dw06uj-2xevmq$ao&l%(i8ua!h+4u1#x2te=&u@d2s9%ky+p)'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
