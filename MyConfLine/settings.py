434e0233aadc23b2a06b6dec1a4dfc7197079da9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 01:55:39 -0400

    fix(settings): fix a typo in INSTALLED_APPS
    Our confline app was registered as  'confline.apps.ConflinConfig' instead of  'confline.apps.ConflineConfig'.

--- MyConfLine/settings.py
+++ MyConfLine/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'confline.apps.ConflinConfig'
+    'confline.apps.ConflineConfig'
 ]
 
 MIDDLEWARE = [
