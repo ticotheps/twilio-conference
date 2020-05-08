8559956e684ac1e6d737cb0fe45768f2979ad601
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 01:13:47 -0400

    feat(settings): add '.ngrok.io' and '127.0.0.1' to ALLOWED_HOSTS

--- MyConfLine/settings.py
+++ MyConfLine/settings.py
@@ -25,7 +25,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.ngrok.io',
+    '127.0.0.1'
+]
 
 
 # Application definition
