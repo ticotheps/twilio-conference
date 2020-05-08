1cf21d9ce870a4c0e80f46e6af246cd000ce1662
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 06:51:54 -0400

    feat(views): add URL for voice recorded call whisper

--- confline/views.py
+++ confline/views.py
@@ -3,6 +3,8 @@ from django.views.decorators.csrf import csrf_exempt
 from decouple import config
 from twilio.twiml.voice_response import VoiceResponse, Say, Dial, Number
 
+CALL_WHISPER_URL = config('CALL_WHISPER_URL')
+
 @csrf_exempt
 def my_conference_line(request):
     response = VoiceResponse()
@@ -14,29 +16,7 @@ def my_conference_line(request):
     response.say(
         'Thank you for allowing TireTutor to make tire buying easier ' +
         'for you! Please wait while we connect you to your tire dealer now!')
-    dial.number(target_number, url='http://demo.twilio.com/docs/classic.mp3')
+    dial.number(target_number, url=CALL_WHISPER_URL)
     response.append(dial)
     
     return HttpResponse(str(response))
-
-# @csrf_exempt
-# def my_conference_line(request):
-#     response = VoiceResponse()
-#     dial = Dial()
-#     response.say(
-#         'Thank you for allowing TireTutor to make tire buying easier ' +
-#         'for you! Please wait while we connect you to your tire dealer now.')
-#     dial.conference(
-#         'TireTutor Conference Room', 
-#         beep=False,
-#         wait_url='http://demo.twilio.com/docs/classic.mp3',
-#         start_conference_on_enter=True,
-#         end_conference_on_exit=True
-#     )
-#     response.append(dial)
-
-#     return HttpResponse(str(response))
-
-# On behalf of the whole TireTutor team, we would just like to say thank you for
-# allowing us to make tire buying easier for you! Please wait while we connect
-# you to your tire dealer now!
