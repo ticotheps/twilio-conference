42119010e720eb9e4018eaa59fb033d04e122861
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 03:53:21 -0400

    refactor(views): remove need to enter PIN number

--- confline/views.py
+++ confline/views.py
@@ -10,27 +10,33 @@ NUM_DIGITS = config("NUM_DIGITS", default=4, cast=int)
 @csrf_exempt
 def my_conference_line(request):
     response = VoiceResponse()
-    
-    if 'Digits' in request.POST:
-        conf_pin = request.POST['Digits']
-    
-        if conf_pin == CONF_PIN:  # <- set desired PIN in .env
-            response.say("Thank you for allowing TireTutor to make tire buying easier for you! Please wait while we connect you to your tire experts.")
-            dial = Dial()
-            dial.conference(
-                'TireTutor Conference Room',
-                max_participants=MAX_PARTICIPANTS,
-                wait_url='http://demo.twilio.com/docs/classic.mp3'
-            )
-            response.append(dial)
-        
-            return HttpResponse(str(response))
-        else:
-            response.say("Sorry, that PIN number is incorrect. Goodbye.")
-    
-    else:
-        gather = Gather(num_digits=NUM_DIGITS)
-        gather.say("Please enter the conference PIN number.")
-        response.append(gather)
+    dial = Dial()
+    response.say("Thank you for allowing TireTutor to make tire buying easier for you! Please wait while we connect you to your tire dealer now.")
+    dial.conference('TireTutor Conference Room', wait_url='http://demo.twilio.com/docs/classic.mp3')
+    response.append(dial)
 
     return HttpResponse(str(response))
+    
+    # if 'Digits' in request.POST:
+    #     conf_pin = request.POST['Digits']
+    
+    #     if conf_pin == CONF_PIN:  # <- set desired PIN in .env
+    #         response.say("Thank you for allowing TireTutor to make tire buying easier for you! Please wait while we connect you to your tire experts.")
+    #         dial = Dial()
+    #         dial.conference(
+    #             'TireTutor Conference Room',
+    #             max_participants=MAX_PARTICIPANTS,
+    #             wait_url='http://demo.twilio.com/docs/classic.mp3'
+    #         )
+    #         response.append(dial)
+        
+    #         return HttpResponse(str(response))
+    #     else:
+    #         response.say("Sorry, that PIN number is incorrect. Goodbye.")
+    
+    # else:
+    #     gather = Gather(num_digits=NUM_DIGITS)
+    #     gather.say("Please enter the conference PIN number.")
+    #     response.append(gather)
+
+    # return HttpResponse(str(response))
