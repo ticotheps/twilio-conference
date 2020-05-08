c36bea9d87dc3248874c1c1d49fc8657adc62d70
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 03:11:59 -0400

    fix(views): The console was unable to execute 'request.POST' because it could not define 'request' until we passed it into the 'my_conference_line( )' method.

--- confline/views.py
+++ confline/views.py
@@ -1,21 +1,21 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from decouple import config
-from twilio.twiml.voice_response import Dial, VoiceResponse
+from twilio.twiml.voice_response import Dial, VoiceResponse, Gather
 
-CONF_PIN = config("CONF_PIN", cast=int)
+CONF_PIN = config("CONF_PIN")
 MAX_PARTICIPANTS = config("MAX_PARTICIPANTS", default=2, cast=int)
 NUM_DIGITS = config("NUM_DIGITS", default=4, cast=int)
 
 @csrf_exempt
-def my_conference_line(self):
+def my_conference_line(request):
     response = VoiceResponse()
     
     if 'Digits' in request.POST:
         conf_pin = request.POST['Digits']
     
         if conf_pin == CONF_PIN:  # <- set desired PIN in .env
-            response.say("Welcome to the TireTutor Conference Room!")
+            response.say("Thank you for allowing TireTutor to make tire buying easier for you! Please wait while we connect you to your tire dealer.")
             dial = Dial()
             dial.conference('TireTutor Conference Room',
                 max_participants=MAX_PARTICIPANTS,
@@ -24,7 +24,7 @@ def my_conference_line(self):
         
             return HttpResponse(str(response))
         else:
-            response.say("Wrong PIN")
+            response.say("Sorry, that PIN number is incorrect. Goodbye.")
     
     else:
         gather = Gather(num_digits=NUM_DIGITS)
