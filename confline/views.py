32a77db7a32d4303e4072edf1cb325d4a44be310
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-05-08 01:36:20 -0400

    feat(views):

--- confline/views.py
+++ confline/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-# Create your views here.
+from twilio.twiml.voice_response import Dial, VoiceResponse
+
+@csrf_exempt
+def my_conference_line(self):
+    response = VoiceResponse()
+    dial = Dial()
+    
+    dial.conference('TireTutor Conference Room')
+    response.append(dial)
+    
+    return HttpResponse(str(response))
